ad71daed74d4663ab552262e90a51615f2d6759e
duyunzhi <duyunzhi@micstack.com>
2022-11-18 10:09:35 +0800

    upgrade to v0.1.1

--- Formula/pdh.rb
+++ Formula/pdh.rb
@@ -1,9 +1,9 @@
 class Pdh < Formula
   desc "send things from one computer to another"
   homepage "https://github.com/duyunzhi/pdh"
-  url "https://github.com/duyunzhi/pdh/releases/download/v0.1.0/pdh-macos.tar.gz"
-  sha256 "4a9a7875737e232cab114ac0022319fd8d88f199cc333249119160a90f32be42"
-  version "0.1.0"
+  url "https://github.com/duyunzhi/pdh/releases/download/v0.1.1/pdh-macos.tar.gz"
+  sha256 "d4e01481e49e45b3c38162ba29b0235d4f38de2c8dc40151bd107b131944500d"
+  version "0.1.1"
   def install
     bin.install "pdh"
   end
