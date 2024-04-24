88047d6427542b93d0981772d376f1ca24bf702e
Yunis <yunis@88.com>
2024-04-24 15:13:04 +0800

    [flash-cat] Fix url is wrong

--- Formula/flash-cat.rb
+++ Formula/flash-cat.rb
@@ -3,10 +3,10 @@ class FlashCat < Formula
     homepage "https://github.com/yunis-du/flash-cat"
   
     if Hardware::CPU.intel?
-      url "https://github.com/yunis-du/flash-cat/releases/download/v0.1.2/flash-cat-cli-macos-amd64.tar.gz"
+      url "https://github.com/yunis-du/flash-cat/releases/download/cli-v0.1.2/flash-cat-cli-macos-amd64.tar.gz"
       sha256 "6f651c63a1f13ba1be1992691c2abadf35151a7ad99dd66ea353ec573d9ae1b3"
     elsif Hardware::CPU.arm?
-      url "https://github.com/yunis-du/flash-cat/releases/download/v0.1.2/flash-cat-cli-macos-arm64.tar.gz"
+      url "https://github.com/yunis-du/flash-cat/releases/download/cli-v0.1.2/flash-cat-cli-macos-arm64.tar.gz"
       sha256 "7d373cf743561dbf44b62730cfc0dc4ac64382ad3f02cc80a9d25019fd5c942b"
     end
   
