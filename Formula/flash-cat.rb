d2662b02a9278734359b11792cc711c9dbc2ce9d
Yunis <yunis@88.com>
2024-09-30 11:52:51 +0800

    [Flash-Cat] Update version to 1.1.0

--- Formula/flash-cat.rb
+++ Formula/flash-cat.rb
@@ -3,14 +3,14 @@ class FlashCat < Formula
     homepage "https://github.com/yunis-du/flash-cat"
   
     if Hardware::CPU.intel?
-      url "https://github.com/yunis-du/flash-cat/releases/download/v1.0.0/flash-cat-cli-macos-amd64.tar.gz"
-      sha256 "ce925ea87bff906875d1bc81d1011b323d75be531fbdd92f33473af2e72be095"
+      url "https://github.com/yunis-du/flash-cat/releases/download/v1.1.0/flash-cat-cli-macos-amd64.tar.gz"
+      sha256 "b4bb34132543f3ca9411104709191887ec06f5a7e87642cbfa7fd14a71c0292c"
     elsif Hardware::CPU.arm?
-      url "https://github.com/yunis-du/flash-cat/releases/download/v1.0.0/flash-cat-cli-macos-arm64.tar.gz"
-      sha256 "2260feebc0c17e56d47e91965fd1c72f57a21d3dc6e8891369771ac04b499213"
+      url "https://github.com/yunis-du/flash-cat/releases/download/v1.1.0/flash-cat-cli-macos-arm64.tar.gz"
+      sha256 "3041f9b36287c3709393b3146631a0b1bc2f4ff45054d8cfe2ec7109233acca2"
     end
   
-    version "1.0.0"
+    version "1.1.0"
   
     def install
       if Hardware::CPU.intel?
