df26bd666204f8b832551a9860a5ab9df2c22dde
Yunis <yunis@88.com>
2024-12-06 19:12:20 +0800

    Flash-Cat] Update version to 1.1.1

--- Formula/flash-cat.rb
+++ Formula/flash-cat.rb
@@ -4,10 +4,10 @@ class FlashCat < Formula
   
     if Hardware::CPU.intel?
       url "https://github.com/yunis-du/flash-cat/releases/download/v1.1.1/flash-cat-cli-macos-1.1.1-x86_64.tar.gz"
-      sha256 "caa8cf83e206bf1aeec8bc7dabf06c09e8031decc9cf6a9ebc74f640fd3d2b72"
+      sha256 "c7f2b0a41ad113728cf5019b8ee98f58129d2c6bfb63e467ed5747bf35a569ca"
     elsif Hardware::CPU.arm?
       url "https://github.com/yunis-du/flash-cat/releases/download/v1.1.1/flash-cat-cli-macos-1.1.1-aarch64.tar.gz"
-      sha256 "6466391f3923fa875bd42bc504595badb8b569eda5da88330c8a6d2a1ab776bc"
+      sha256 "ac1e8427d6cf84f820436e2ed263e0b3b73ad22bf62f5f74a6347f2f671706dc"
     end
   
     version "1.1.0"
