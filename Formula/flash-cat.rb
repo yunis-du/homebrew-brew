e7e1c4a3fbbf718d73873aaca04e2518b932b159
Yunis <yunis@88.com>
2024-12-09 16:07:25 +0800

    Modify sha256

--- Formula/flash-cat.rb
+++ Formula/flash-cat.rb
@@ -4,10 +4,10 @@ class FlashCat < Formula
   
     if Hardware::CPU.intel?
       url "https://github.com/yunis-du/flash-cat/releases/download/v1.1.2/flash-cat-cli-macos-1.1.2-x86_64.tar.gz"
-      sha256 "c7f2b0a41ad113728cf5019b8ee98f58129d2c6bfb63e467ed5747bf35a569ca"
+      sha256 "ccab1b6b37f16d8cdb407f7073095edef9797978f34164bf669016dafd610fb7"
     elsif Hardware::CPU.arm?
       url "https://github.com/yunis-du/flash-cat/releases/download/v1.1.2/flash-cat-cli-macos-1.1.2-aarch64.tar.gz"
-      sha256 "ac1e8427d6cf84f820436e2ed263e0b3b73ad22bf62f5f74a6347f2f671706dc"
+      sha256 "a5c53ec3af88b60bd4bbc1bb3f9244973b085e5c46be8bb3876a3432935b8476"
     end
   
     version "1.1.2"
