92c6ced91e809df8df160df787d6aff64211a2bc
Yunis <yunis@88.com>
2025-02-14 17:26:25 +0800

    Update Flash-Cat SHA256 checksums for version 2.0.5

--- Formula/flash-cat.rb
+++ Formula/flash-cat.rb
@@ -4,10 +4,10 @@ class FlashCat < Formula
   
     if Hardware::CPU.intel?
       url "https://github.com/yunis-du/flash-cat/releases/download/v2.0.5/flash-cat-cli-macos-2.0.5-x86_64.tar.gz"
-      sha256 "7cd3efd8bd198bb27577de52f6befbf66b58b99ef8f9b7c24eef899561de904c"
+      sha256 "cb1ad678ea2e8c97ee26abefebe11649c58d8e4fc02df032e88ce106174f6f6f"
     elsif Hardware::CPU.arm?
       url "https://github.com/yunis-du/flash-cat/releases/download/v2.0.5/flash-cat-cli-macos-2.0.5-aarch64.tar.gz"
-      sha256 "8e608bc7db70de18c351433543422f19969b3900d0e32488db6562e764ffed64"
+      sha256 "6e2f3baec9d03365fafcc8f50dc0eb7029a2c919c046d69592c174c0dbf73744"
     end
   
     version "2.0.5"
