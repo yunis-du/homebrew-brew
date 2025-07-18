8744ad17bcadc23316a45f082c3e1e84c3d05ece
Yunis <yunis@88.com>
2025-07-18 13:21:36 +0800

    [Flash-Cat] Upgrade to version 2.1.1

--- Formula/flash-cat.rb
+++ Formula/flash-cat.rb
@@ -1,16 +1,16 @@
 class FlashCat < Formula
     desc "Send things from one computer to another"
     homepage "https://github.com/yunis-du/flash-cat"
+    version "2.1.1"
   
     if Hardware::CPU.intel?
-      url "https://github.com/yunis-du/flash-cat/releases/download/v2.1.0/flash-cat-cli-macos-2.1.0-x86_64.tar.gz"
-      sha256 "3bb32b1581b62006c3a9954028a5fd1bf232fca66e54a9cef609d4ee8c8d2978"
+      url "https://github.com/yunis-du/flash-cat/releases/download/v#{version}/flash-cat-cli-macos-#{version}-x86_64.tar.gz"
+      sha256 "485a7cc2c8eef97feeee7b84398e02518c9a4e54449c2aa1ef8933becd1cb6b6"
     elsif Hardware::CPU.arm?
-      url "https://github.com/yunis-du/flash-cat/releases/download/v2.1.0/flash-cat-cli-macos-2.1.0-aarch64.tar.gz"
-      sha256 "81076235b65a9323f5301fcf3778ef9c0b6a5ddd08d620795483791338df5b02"
+      url "https://github.com/yunis-du/flash-cat/releases/download/v#{version}/flash-cat-cli-macos-#{version}-aarch64.tar.gz"
+      sha256 "fec3fa20ba8c51870c215f3558c8788072fc65379b213eea382b4e21e3133c3d"
     end
   
-    version "2.1.0"
   
     def install
       if Hardware::CPU.intel?
