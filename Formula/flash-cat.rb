9818b62bbf9da50a751e488471ecf75c2c74784f
Yunis <yunis@88.com>
2025-12-25 11:49:51 +0800

    [Flash-Cat] Upgrade to version 2.1.8

--- Formula/flash-cat.rb
+++ Formula/flash-cat.rb
@@ -1,14 +1,14 @@
 class FlashCat < Formula
     desc "Send things from one computer to another"
     homepage "https://github.com/yunis-du/flash-cat"
-    version "2.1.7"
+    version "2.1.8"
   
     if Hardware::CPU.intel?
       url "https://github.com/yunis-du/flash-cat/releases/download/v#{version}/flash-cat-cli-macos-#{version}-x86_64.tar.gz"
-      sha256 "eb057d3c5fefb21a224fec6f3551af41b9906392c9ace1a7e103c4812b2e6676"
+      sha256 "51a44a1ca899e6365a257267d468a0fa82af3dc8cac33f734b72af88934f3314"
     elsif Hardware::CPU.arm?
       url "https://github.com/yunis-du/flash-cat/releases/download/v#{version}/flash-cat-cli-macos-#{version}-aarch64.tar.gz"
-      sha256 "7921895697ce5de6622e49412256c6ee683c0151c2e9c41d9663af4003155acd"
+      sha256 "33030ab5b467c2f807537c1942fbc0d90341b6044350ba50c8999dc020b431a2"
     end
   
   
